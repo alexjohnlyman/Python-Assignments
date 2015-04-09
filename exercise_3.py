a3528f15d8e7611971f5d91cc2c2c6c03590f73b
Alex Lyman <alexjohnlyman@gmail.com>
2015-04-08 22:58:18 -0600

    Homework 3 - finished option 1

--- exercise_3.py
+++ exercise_3.py
@@ -4,20 +4,19 @@ mph = float(raw_input("Please enter a speed in miles/hour: "))
 #Conversions
 meter_hour = mph * 1609.34
 barleycorn_day = meter_hour * 24
-furlong =
-CarDi = mph * 20
-energy = (mph * 115000)/75700
-cost = mph * 4
+furlong_hour = mph * 1760
+fortnight = 336 #hours in a fortnight
+furlong_fort = furlong_hour * fortnight
+mach = (((mph/60)/60)/5280)  #feet in a mile = 5280
+PSL = ((meter_hour/60)/299792458)
 
 if mph == 0:
     mph = float(raw_input("You cannot use 0. Please enter a speed in miles/hour: "))
-
-print "Original speed in mph is: %f" %(mph)
-print "%f mph is equivalent of %f liters" %(mph, liter)
-print "%f mph of gasoline requires %f barrels of oil" %(mph, barrel)
-print "%f mph of gasoline produces %f pounds of CO2" %(mph, CarDi)
-print "%f mph of gasoline is energy equivalent to %f mph of ethanol" %(mph, energy)
-print "%f mph of gasoline requires $%f US dollars" %(mph, cost)
+print "Original speed in mph is: %.2f" %(mph)
+print "Converted to barleycorn/day is: %.2e" %(barleycorn_day)
+print "Converted to furlongs per fortnight is: %.2f" %(furlong_fort)
+print "Converted to Mach number is: %.2f" %(mach)
+print "Converted to the percentage of the speed of light is: %.2e" %(PSL)
 
 
 # # option 2
