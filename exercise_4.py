b1aa992e9836508a71abb2d186610f614641196e
Alex Lyman <alexjohnlyman@gmail.com>
2015-04-09 23:20:12 -0600

    Finished HW 4

--- exercise_4.py
+++ exercise_4.py
@@ -14,10 +14,13 @@
 # new_population = (population + total_increase)
 # print "New population in %i years will change by %.2f to be %.2f" % (years, total_increase, new_population)
 
-# Option 2
+
+# Option 2 with letter, 0, ".", and "enter" check
 def get_input(message):
     user_input = str(raw_input(message))
-    if user_input.isdigit() and user_input is not "0" and user_input is not "" and user_input is not ".":
+    if user_input < "1":
+        print("Cannot be 0 or a letter!")
+    elif user_input.isdigit() and user_input is not 0 and user_input is not "" and user_input is not ".":
         user_input = float(user_input)
         return user_input
     else:
@@ -25,7 +28,6 @@ def get_input(message):
     return None
 
 years = None
-
 while years is None:
     years = get_input("Please enter an amount of years into the future: ")
 
@@ -37,43 +39,3 @@ immigrant_rate = (seconds/35)
 total_increase = ((birth_rate + immigrant_rate) - death_rate)
 new_population = (population + total_increase)
 print "New population in %i years will change by %.2f to be %.2f" % (years, total_increase, new_population)
-
-# jared solution to look at
-# def get_input(message):
-#         user_input = str(raw_input(message))
-#         sanitized_input = ""
-#         dot_count = 0
-#         for char in user_input:
-#             if char.isdigit() or (char is "." and dot_count < 1):
-#                 if char is ".":
-#                     dot_count += 1
-#                 sanitized_input = str(sanitized_input + char)
-#             elif char is ".":
-#                 dot_count += 1
-#         if sanitized_input is not "0" and sanitized_input is not "" and sanitized_input is not ".":
-#             sanitized_input = float(sanitized_input)
-#             if dot_count > 1:
-#                 print("You entered multiple '.' so we assumed you meant only the first one. ;)")
-#             print "Your original input was '{}' but we extracted {} gallons.".format(user_input, sanitized_input)
-#             return sanitized_input
-#         print("Cannot be 0 or a letter!")
-#         return None
-#
-# gallons = None
-#
-# while gallons is None:
-#     gallons = get_input("Please input the number of gallons: ")
-#
-#
-# liters = gallons * 3.7854
-# barrels = round(gallons / 19.5, 2)
-# co2 = round(gallons * 20, 2)
-# ethanol = round((gallons * 115) / 75.7, 2)
-# price = round(gallons * 4.00, 2)
-#
-# print "Total liters: {} liters".format(liters)
-# print "Number of barrels of oil required: {} barrels".format(barrels)
-# print "Number of pounds of CO2 produced: {}lbs".format(co2)
-# print "Equivalent energy amount of ethanol gas: {} gallons. That is a difference of {} gallons."\
-#     .format(ethanol, round(ethanol - gallons, 2))
-# print "Total cost: ${}".format(price)
