0039a0f2a0f5e108c32fe7f325deb34c6382a13d
Alex Lyman <alexjohnlyman@gmail.com>
2015-04-09 19:51:25 -0600

    Homework 4 and update to HW 3

--- exercise_4.py
+++ exercise_4.py
@@ -1,7 +1,41 @@
 
 #Option 1 - without the letter check
-# mph = float(raw_input("Please enter a speed in miles/hour: "))
-# meter_hour = mph * 1609.34
+years = raw_input("How many years: ")
+
+population = 307357870
+seconds = (((((years)*365)*24)*60)*60)
+birth_rate = population +=
+death_rate =
+immigrant_rate =
+total_increase = (birth_rate + immigrant_rate) - death_rate
+new_population = (population += total_increase)
+
+
+
+if mph == 0:
+    mph = float(raw_input("You cannot use 0. Please enter a speed in miles/hour: "))
+print "New population in %i years will change by %f to be %f" %(years, total_increase, new_population)
+
+
+
+
+
+
+
+
+
+# # option 2 letter check
+# def get_info():
+#     mph = raw_input("Please enter a speed in miles/hour: ")
+#     if mph.isdigit() and float(mph) > 0:
+#         return mph
+#     else:
+#         print "You can't enter 0 or a word. Try again."
+#         return get_info()
+#
+#
+# mph = get_info()
+# meter_hour = float(mph) * 1609.34
 # barleycorn_day = meter_hour * 24
 # furlong_hour = mph * 1760
 # fortnight = 336 #hours in a fortnight
@@ -9,40 +43,12 @@
 # mach = (((mph/60)/60)/5280)  #feet in a mile = 5280
 # PSL = ((meter_hour/60)/299792458)
 #
-# if mph == 0:
-#     mph = float(raw_input("You cannot use 0. Please enter a speed in miles/hour: "))
 # print "Original speed in mph is: %.2f" %(mph)
 # print "Converted to barleycorn/day is: %.2e" %(barleycorn_day)
 # print "Converted to furlongs per fortnight is: %.2f" %(furlong_fort)
 # print "Converted to Mach number is: %.2f" %(mach)
 # print "Converted to the percentage of the speed of light is: %.2e" %(PSL)
-
-
-# # option 2 letter check
-def get_info():
-    mph = raw_input("Please enter a speed in miles/hour: ")
-    if mph.isdigit() and float(mph) > 0:
-        return mph
-    else:
-        print "You can't enter 0 or a word. Try again."
-        return get_info()
-
-
-mph = get_info()
-meter_hour = float(mph) * 1609.34
-barleycorn_day = meter_hour * 24
-furlong_hour = mph * 1760
-fortnight = 336 #hours in a fortnight
-furlong_fort = furlong_hour * fortnight
-mach = (((mph/60)/60)/5280)  #feet in a mile = 5280
-PSL = ((meter_hour/60)/299792458)
-
-print "Original speed in mph is: %.2f" %(mph)
-print "Converted to barleycorn/day is: %.2e" %(barleycorn_day)
-print "Converted to furlongs per fortnight is: %.2f" %(furlong_fort)
-print "Converted to Mach number is: %.2f" %(mach)
-print "Converted to the percentage of the speed of light is: %.2e" %(PSL)
-
+#
 # jared solution to look at
 # def get_input(message):
 #         user_input = str(raw_input(message))
