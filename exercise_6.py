99e284d2b8841036d34c261727e1b6c95ab2a5ef
Alex Lyman <alexjohnlyman@gmail.com>
2015-04-17 13:49:35 -0600

    Updated Exercise 6

--- exercise_6.py
+++ exercise_6.py
@@ -1,18 +1,43 @@
-# A good place to start:
-
 # Excercise number 6
-when you call your functions one of them will have a try except wrapped in there
+# when you call your functions one of them will have a try except wrapped in there
+
+# open function
 def open_me(filename):
     with open(filename) as f:
-        (more code)
+        text = f.read()
+        print text
 
-try:
-    open_me(filename)
+# # Try function
+def try_me(filename):
+    try:
+        open_me(filename)
+    except IOError:
+        print "File does not exist. Please choose another file."
 
-except:
-    IOError
+
+
+# include the try_except and open_me in here
+def try_and_open_me(filename):
+    open_me(filename):
+    def try_me(filename):
+
+try_me("exercise_five_hive")
 
 
 
-def open_me_try_except()
-#     include the try_except in here
+
+# Examples
+# try:
+#     fh = open("/tmp/blah/blah/blah", "w")
+#     fh.write("This is my test file for exception handling!!")
+# except IOError:
+#     print "Error: can't find file or read data"
+# else:
+#     print "Written content in the file successfully"
+#
+# # An example with a finally block, without an except block
+# try:
+#     fh = open("/tmp/blah/blah/testfile", "w")
+#     fh.write("This is my test file for exception handling!!")
+# finally:
+#     print "Error: can't find file or read data"
