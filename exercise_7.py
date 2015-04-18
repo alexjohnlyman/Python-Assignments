bbcfcaedd5034fbe81e8f155fdaa5320be9a5618
Alex Lyman <alexjohnlyman@gmail.com>
2015-04-18 15:53:38 -0600

    Started HW 7

--- exercise_7.py
+++ exercise_7.py
@@ -1,6 +1,2 @@
-# You have to add a __init__.py file to the folder to make that folder a module which you can import into another file.
-
-
+# I will have two functions (c->f) and (c<-f)
 # Exercise 7
-
-    # you will have two functions (c->f) and (c<-f)
---
+++ exercise_7_temperature.py
@@ -0,0 +1,5 @@
+from exercise_7 import c2f, f2c
+
+c2f()
+
+f2c()
